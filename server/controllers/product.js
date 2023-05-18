725740861965b6097c6f03c679b8e883c636a766
nguyenngocthanh229 <hei2292001@gmail.com>
2023-05-18 09:48:01 +0700

    update

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -94,19 +94,32 @@ const ratings = asyncHandler(async(req, res) => {
     const {star, comment, pid} = req.body
     if (!star || !pid) throw new Error('Missing inputs')
     const ratingProduct = await Product.findById(pid)
-    const alreadyRating = ratingProduct?.ratings?.some(el => el.postedBy.some(uid => uid === _id))
-    console.log({ alreadyRating });
+    const alreadyRating = ratingProduct?.ratings?.find(el => el.postedBy.toString() === _id)
     if (alreadyRating) {
        
-    }else{
-        const response = await Product.findByIdAndUpdate(pid, {
+    await Product.updateOne({
+        ratings: { $elemMatch:alreadyRating }
+    }, {
+        $set: { "ratings.$.star": star, "ratings.$.comment": comment }
+    }, {new: true})
+    } else {
+        await Product.findByIdAndUpdate(pid, {
             $push: {ratings: {star, comment, postedBy: _id}}
         }, {new: true})
-        console.log(response);
     }
 
+    // Sum ratings
+    const updatedProduct = await Product.findById(pid)
+    const ratingCount = updatedProduct.ratings.length
+    const sumRatings = updatedProduct.ratings.reduce((sum, el) => sum + +el.star, 0)
+    updatedProduct.totalRatings = Math.round(sumRatings * 10/ratingCount) / 10
+
+    await updatedProduct.save()
+
+
     return res.status(200).json({
-        status: true
+        status: true,
+        updatedProduct
     })
  }) 
 module.exports = {
