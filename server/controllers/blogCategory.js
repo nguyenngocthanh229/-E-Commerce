163066d2180a1651a43a8a184fa135a49ab3fdd9
nguyenngocthanh229 <hei2292001@gmail.com>
2023-05-18 10:43:26 +0700

    update

--- server/controllers/blogCategory.js
+++ server/controllers/blogCategory.js
@@ -12,20 +12,20 @@ const getCategories = asyncHandler(async(req, res) => {
     const response = await BlogCategory.find().select('title _id')
     return res.json({
         success: response ? true : false,
-        prodCategories: response ? response : 'Cannot get blog-category'
+        blogCategories: response ? response : 'Cannot get blog-category'
     })
 })
 const updateCategory = asyncHandler(async(req, res) => { 
-    const {pcid} = req.params
-    const response = await BlogCategory.findByIdAndUpdate(pcid, req.body, {new: true})
+    const {bcid} = req.params
+    const response = await BlogCategory.findByIdAndUpdate(bcid, req.body, {new: true})
     return res.json({
         success: response ? true : false,
         updatedCategory: response ? response : 'Cannot update blog-category'
     })
 })
 const deleteCategory = asyncHandler(async(req, res) => { 
-    const {pcid} = req.params
-    const response = await BlogCategory.findByIdAndDelete(pcid)
+    const {bcid} = req.params
+    const response = await BlogCategory.findByIdAndDelete(bcid)
     return res.json({
         success: response ? true : false,
         deleteCategory: response ? response : 'Cannot delete blog-category'
